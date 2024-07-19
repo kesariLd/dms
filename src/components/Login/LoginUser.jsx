9d89d696f1a9f706ed641bfeffc523784a40ea80
kesariLd <kesari.patro@ldtech.in>
2024-07-19 17:06:12 +0530

    mui added

--- src/components/Login/LoginUser.jsx
+++ src/components/Login/LoginUser.jsx
@@ -1,4 +1,13 @@
+import  { useState } from 'react';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+
 const LoginUser = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -33,15 +42,26 @@ const LoginUser = () => {
               <label htmlFor="password" className="block text-sm font-medium text-gray-800">
                 Password
               </label>
-              <div className="mt-1">
+              <div className="mt-1 relative">
                 <input
                   id="password"
                   name="password"
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   autoComplete="current-password"
                   required
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm pr-10"
                 />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={togglePasswordVisibility}
+                >
+                  {showPassword ? (
+                    <VisibilityOff className="h-5 w-5 text-gray-400" />
+                  ) : (
+                    <Visibility className="h-5 w-5 text-gray-400" />
+                  )}
+                </button>
               </div>
             </div>
 
